46db7a913ae525e465f734830591561f9c6620d5
Fuli-Lin <flin0022@student.monash.edu>
2025-08-06 12:21:29 +1000

    Initial commit

---
+++ src/components/MyName.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p class="my-name">{{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName',
+  data() {
+    return {
+      name: 'Hello, My name is Fuli Lin!',
+    }
+  },
+}
+</script>
+
+<style scoped>
+.my-name {
+  padding-top: 20px;
+  font-size: 18px;
+  color: red;
+}
+</style>
