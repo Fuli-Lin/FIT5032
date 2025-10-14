7b02be3c330fbad0b0b9bbd277c5badb4be0bdb7
Fuli-Lin <flin0022@student.monash.edu>
2025-10-14 17:30:13 +1100

    update button text

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -4,7 +4,7 @@
       <h1>WEATHER APP</h1>
       <div class="search-bar">
         <input type="text" v-model="city" placeholder="Enter city name" class="search-input" />
-        <button @click="searchByCity" class="search-button">Search</button>
+        <button @click="searchByCity" class="search-button">Search !</button>
       </div>
     </div>
   </div>
