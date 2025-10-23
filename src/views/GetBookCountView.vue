c9361dc79d36b143f45c82bbdb2cac50a20f5aad
Fuli-Lin <flin0022@student.monash.edu>
2025-10-23 11:45:17 +1100

    change text

--- src/views/GetBookCountView.vue
+++ src/views/GetBookCountView.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1>Book Count</h1>
-    <button @click="getBookCount">Get Book Count</button>
+    <button @click="getBookCount">Get Book Counts</button>
     <p v-if="count !== null">Total number of books: {{ count }}</p>
     <p v-if="error">{{ error }}</p>
   </div>
