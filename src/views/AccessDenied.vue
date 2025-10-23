1fa6905fa8a7ffdbd6424d8093ae62bc1edd2b68
Fuli-Lin <flin0022@student.monash.edu>
2025-10-23 01:18:09 +0000

    Deploying to gh-pages from @ Fuli-Lin/FIT5032@40dd8e802921b2b05978435540ca73352fd3c62e 🚀

--- src/views/AccessDenied.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="denied">
-    <h2>Access Denied</h2>
-    <p>You must be logged in to view this page.</p>
-    <router-link to="/login">Go to Login</router-link>
-  </div>
-</template>
